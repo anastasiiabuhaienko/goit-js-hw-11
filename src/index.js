371234ea6e19a5fbac09d3c744d29ec3af7cbeeb
Anastasiia Buhaienko <anastasiia.buhaienko@gmail.com>
2023-04-06 04:59:29 +0300

    add async

--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
-import './css/styles.css';
+import './sass/styles.scss';
 
 import axios from "axios";
 import Notiflix from "notiflix";
 
 
-const marckupGalleryCard = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 const submitBtn = document.querySelector('button');
 
 submitBtn.addEventListener('submit', photoSerch);
 
+// получаем массив фоток и на их основании добавляем разметку в чать Gallery
+function photoSerch(photos) {
 
-function photoSerch() {
+   const markupCard = photos.map({webformatURL, largeImageURL, tags, like, views, comments, downloads}) => {
+    return `
+    <div class="photo-card">
+    <a class="photo-card__link" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+    <p class="info-item">
+        <b>Likes</b>
+        <span>${like}</span>
 
-    marckupGalleryCard.innerHTML = `Hello`;
-    //  `<div class="photo-card">
-    // <img src="" alt="" loading="lazy" />
-    // <div class="info">
-    // <p class="info-item">
-    //     <b>Likes</b>
-    // </p>
-    // <p class="info-item">
-    //     <b>Views</b>
-    // </p>
-    // <p class="info-item">
-    //     <b>Comments</b>
-    // </p>
-    // <p class="info-item">
-    //     <b>Downloads</b>
-    // </p>
-    // </div>
-    // </div>`;
+    </p>
+    <p class="info-item">
+        <b>Views</b>
+        <span>${views}</span>
+    </p>
+    <p class="info-item">
+        <b>Comments</b>
+        <span>${comments}</span>
+    </p>
+    <p class="info-item">
+        <b>Downloads</b>
+        <span>${downloads}</span>
+    </p>
+    </div>
+    </div>`;
+   }
+   gallery.insertAdjacentElement('beforeend', markupCard);
 };
+
+
+
+const API_KEY = '17189490-14419ab63c20cc5cd4901fe08';
+
+
+    async function fetchPhotos(q, page) {
+        const response = await axios.get(`
+        https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1
+        `);  
+        return await response.data;
+      };
 
