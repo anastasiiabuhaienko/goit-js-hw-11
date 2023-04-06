f082879b39b427ecc5c082ab9e9db06b3cf59364
Anastasiia Buhaienko <anastasiia.buhaienko@gmail.com>
2023-04-07 02:36:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import './sass/styles.scss';
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from "axios";
 import Notiflix from "notiflix";
+
 
 const gallery = document.querySelector('.gallery');
 const searchQueryForm = document.getElementById('search-form');
 const searchQuery = document.getElementById('search-query');
 const loadMoreBtn = document.querySelector('.load-more');
 
-let defaultEmptySearchField = '';
+// let defaultEmptySearchField = '';
 
-searchQueryForm .addEventListener('submit', formSubmitSent);
+
+let page = 1;
+const lightbox = new SimpleLightbox('.gallery a',
+  {
+    captionsData: 'alt',
+    captionPosition: 'outside',
+    captionDelay: 250,
+  });
+
+
+searchQueryForm.addEventListener('submit', formSubmitSent);
+loadMoreBtn.addEventListener('click', loadMore);
 
 // получаем массив фоток и на их основании добавляем разметку в чать Gallery
 function photoSerch(photos) {
@@ -43,7 +56,11 @@ function photoSerch(photos) {
     </div>`
    }).join('');
    gallery.insertAdjacentHTML('beforeend', markupCard);
+   lightbox.refresh();
 };
+
+
+
 
 // функция очистки формы при поиске по новому ключевому слову
 
@@ -58,9 +75,11 @@ const API_KEY = '17189490-14419ab63c20cc5cd4901fe08';
 
 
     async function fetchPhotos(q, page) {
-        return await axios.get(
+        let result = await axios.get(
             `https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-        );  
+        );
+
+        return await result.data;
       };
 
 function formSubmitSent (event) {
@@ -68,14 +87,31 @@ function formSubmitSent (event) {
     event.preventDefault(); 
 
     clearGallery();
+    page = 1;
+    loadImages();
+};
 
-    defaultEmptySearchField = event.currentTarget.searchQuery.value.trim();
-       
+function loadImages() {
+    let defaultEmptySearchFieldValue = searchQuery.value.trim();
 
-    fetchPhotos(searchQuery.value.trim(), 1).then((response) => {
-        photoSerch(response.data.hits);
-    });
-    if (defaultEmptySearchField === '') {    
-        return Notiflix.Notify.info('Your query is empty, please enter data to search.');    
-      };  
+    if (defaultEmptySearchFieldValue != '') {
+        fetchPhotos(defaultEmptySearchFieldValue, page).then((response) => {
+            if (response.hits.length > 0) {
+                photoSerch(response.hits);
+                loadMoreBtn.classList.remove('is-hidden');
+
+            } else {
+                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            }
+        });
+    } else {
+        Notiflix.Notify.info('Your query is empty, please enter data to search.');
+    }
+};
+
+function loadMore() {
+    page++;
+    loadImages();
+
 }
+
